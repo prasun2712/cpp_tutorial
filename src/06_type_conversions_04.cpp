c41b893d21b498f3633067a4b1411d6bdb8276e1
Prasun <prasun.ieee@gmail.com>
2024-09-26 23:11:39 +0530

    Added information for static cast along with dynamic cast.

--- src/06_type_conversions_04.cpp
+++ src/06_type_conversions_04.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 /*
+Dynamic Cast
+============
 dynamic_cast can only be used with pointers and references to classes (or with void*). 
 Its purpose is to ensure that the result of the type conversion points to a valid complete object of the destination pointer type.
 
@@ -63,4 +65,33 @@ an exception of type bad_cast is thrown instead.
 
 dynamic_cast can also perform the other implicit casts allowed on pointers: casting null pointers between pointers types (even between unrelated classes), 
 and casting any pointer of any type to a void* pointer.
+*/
+
+/*
+Static Cast
+===========
+static_cast can perform conversions between pointers to related classes, not only upcasts (from pointer-to-derived to pointer-to-base), 
+but also downcasts (from pointer-to-base to pointer-to-derived). No checks are performed during runtime to guarantee that 
+the object being converted is in fact a full object of the destination type. Therefore, it is up to the programmer to ensure that the conversion is safe. 
+On the other side, it does not incur the overhead of the type-safety checks of dynamic_cast.
+ - class Base {};
+ - class Derived: public Base {};
+ - Base * a = new Base;
+ - Derived * b = static_cast<Derived*>(a);
+
+This would be valid code, although b would point to an incomplete object of the class and could lead to runtime errors if dereferenced.
+
+Therefore, static_cast is able to perform with pointers to classes not only the conversions allowed implicitly, but also their opposite conversions.
+
+static_cast is also able to perform all conversions allowed implicitly (not only those with pointers to classes), 
+and is also able to perform the opposite of these. It can:
+ * Convert from void* to any pointer type. In this case, it guarantees that if the void* value was obtained by converting from that same pointer type, 
+ the resulting pointer value is the same.
+ * Convert integers, floating-point values and enum types to enum types.
+
+Additionally, static_cast can also perform the following:
+ * Explicitly call a single-argument constructor or a conversion operator.
+ * Convert to rvalue references.
+ * Convert enum class values into integers or floating-point values.
+ * Convert any type to void, evaluating and discarding the value.
 */
