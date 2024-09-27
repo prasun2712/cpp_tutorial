f6ebfdcea4557539abb17815d3884e5efbfef793
Prasun <prasun.ieee@gmail.com>
2024-09-27 17:26:18 +0530

    Added information for reinterpret cast.

--- src/06_type_conversions_04.cpp
+++ src/06_type_conversions_04.cpp
@@ -95,3 +95,25 @@ Additionally, static_cast can also perform the following:
  * Convert enum class values into integers or floating-point values.
  * Convert any type to void, evaluating and discarding the value.
 */
+
+/*
+Reinterpret Cast
+================
+reinterpret_cast converts any pointer type to any other pointer type, even of unrelated classes. 
+The operation result is a simple binary copy of the value from one pointer to the other. 
+All pointer conversions are allowed: neither the content pointed nor the pointer type itself is checked.
+
+It can also cast pointers to or from integer types. The format in which this integer value represents a pointer is platform-specific. 
+The only guarantee is that a pointer cast to an integer type large enough to fully contain it (such as intptr_t), 
+is guaranteed to be able to be cast back to a valid pointer.
+
+The conversions that can be performed by reinterpret_cast but not by static_cast are low-level operations based on reinterpreting the binary 
+representations of the types, which on most cases results in code which is system-specific, and thus non-portable. For example:
+ - class A { Code };
+ - class B { Code };
+ - A * a = new A;
+ - B * b = reinterpret_cast<B*>(a);
+
+This code compiles, although it does not make much sense, since now b points to an object of a totally unrelated and likely incompatible class. 
+Dereferencing b is unsafe.
+*/
