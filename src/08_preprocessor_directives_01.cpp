9c00833bfff1205e738d40b6e74006db5c1cfb79
Prasun <prasun.ieee@gmail.com>
2024-09-29 12:22:35 +0530

    Added information on defining and undefining macros + two special operators (# and ##).

--- src/08_preprocessor_directives_01.cpp
+++ src/08_preprocessor_directives_01.cpp
@@ -36,6 +36,38 @@ exactly as you would expect if it was a function:
 */
 #define getmax(a,b) ((a)>(b)?(a):(b))
 
+/*
+Defined macros are not affected by block structure. A macro lasts until it is undefined with the #undef preprocessor directive:
+ - #define TABLE_SIZE 100
+ - int table1[TABLE_SIZE];
+ - #undef TABLE_SIZE
+ - #define TABLE_SIZE 200
+ - int table2[TABLE_SIZE];
+
+This would generate the same code as:
+ - int table1[100];
+ - int table2[200];
+
+Function macro definitions accept two special operators (# and ##) in the replacement sequence:
+The operator #, followed by a parameter name, is replaced by a string literal that contains the argument passed (as if enclosed between double quotes):
+ - #define str(x) #x
+ - cout << str(test);
+
+This would be translated into:
+ - cout << "test";
+
+The operator ## concatenates two arguments leaving no blank spaces between them:
+ - #define glue(a,b) a ## b
+ - glue(c,out) << "test";
+
+This would also be translated into:
+ - cout << "test";
+
+Because preprocessor replacements happen before any C++ syntax check, macro definitions can be a tricky feature. 
+But, be careful: code that relies heavily on complicated macros become less readable, 
+since the syntax expected is on many occasions different from the normal expressions programmers expect in C++.
+*/
+
 int main()
 {
   int x=5, y;
@@ -44,3 +76,4 @@ int main()
   cout << getmax(7,x) << endl;
   return 0;
 }
+
