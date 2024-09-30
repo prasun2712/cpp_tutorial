f7fc27ac8a85a2456f05dbd0b43ccdebb3b6f0b3
Prasun <prasun.ieee@gmail.com>
2024-09-30 22:06:05 +0530

    Added information for opening a file.

--- src/09_io_files_01.cpp
+++ src/09_io_files_01.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 /*
+Input/output with files
+=======================
 C++ provides the following classes to perform output and input of characters to/from files:
 
     ofstream    : Stream class to write on files
@@ -27,3 +29,59 @@ int main()
     myfile.close();
     return 0;
 }
+
+/*
+Open a file
+===========
+The first operation generally performed on an object of one of these classes is to associate it to a real file. This procedure is known as to open a file. 
+An open file is represented within a program by a stream (i.e., an object of one of these classes; in the previous example, this was myfile) and 
+any input or output operation performed on this stream object will be applied to the physical file associated to it.
+
+In order to open a file with a stream object we use its member function open:
+ - open (filename, mode);
+
+Where filename is a string representing the name of the file to be opened, and mode is an optional parameter with a combination of the following flags:
+=================================================================================================================================================
+ios::           in	Open for input operations.
+ios::out	    Open for output operations.
+ios::binary	    Open in binary mode.
+ios::ate	    Set the initial position at the end of the file.
+                If this flag is not set, the initial position is the beginning of the file.
+ios::app	    All output operations are performed at the end of the file, appending the content to the current content of the file.
+ios::trunc	    If the file is opened for output operations and it already existed, its previous content is deleted and replaced by the new one.
+=================================================================================================================================================
+
+All these flags can be combined using the bitwise operator OR (|). 
+For example, if we want to open the file example.bin in binary mode to add data we could do it by the following call to member function open:
+ - ofstream myfile;
+ - myfile.open ("example.bin", ios::out | ios::app | ios::binary);
+
+Each of the open member functions of classes ofstream, ifstream and fstream has a default mode that is used if the file is opened without a second argument:
+==================================
+class	    default mode parameter
+==================================
+ofstream	ios::out
+ifstream	ios::in
+fstream	    ios::in | ios::out
+==================================
+
+For ifstream and ofstream classes, ios::in and ios::out are automatically and respectively assumed, 
+even if a mode that does not include them is passed as second argument to the open member function (the flags are combined).
+
+For fstream, the default value is only applied if the function is called without specifying any value for the mode parameter. 
+If the function is called with any value in that parameter the default mode is overridden, not combined.
+
+File streams opened in binary mode perform input and output operations independently of any format considerations. 
+Non-binary files are known as text files, and some translations may occur due to formatting of some special characters (like newline and carriage return characters).
+
+Since the first task that is performed on a file stream is generally to open a file, 
+these three classes include a constructor that automatically calls the open member function and has the exact same parameters as this member.
+Therefore, we could also have declared the previous myfile object and conduct the same opening operation in our previous example by writing:
+ - ofstream myfile ("example.bin", ios::out | ios::app | ios::binary);
+
+Combining object construction and stream opening in a single statement. Both forms to open a file are valid and equivalent.
+
+To check if a file stream was successful opening a file, you can do it by calling to member is_open. 
+This member function returns a bool value of true in the case that indeed the stream object is associated with an open file, or false otherwise:
+ - if (myfile.is_open()) { ok, proceed with output. }
+*/
